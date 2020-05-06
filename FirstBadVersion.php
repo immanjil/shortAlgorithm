47c7281a108930f8ff459fc623b3742f26ad74a4
Manjil Thapa Magar <manjil.magar@powerwizard.com>
2020-05-05 20:58:32 -0500

    Changes to add test cases to first bad version algorithm

--- FirstBadVersion.php
+++ FirstBadVersion.php
@@ -1,5 +1,23 @@
 <?php
+
 /**
+ * returns if version is bad
+ *
+ * @param $n
+ * @return bool
+ */
+function isBadVersion($n)
+{
+    $badVersion = range(4, 1000);
+    if (in_array($n, $badVersion)) {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * returns first bad version for given version
+ *
  * @param Integer $n
  * @return Integer
  */
@@ -8,12 +26,21 @@ function firstBadVersion($n)
     $left = 1;
     $right = $n;
     while ($left < $right) {
+
         $mid = floor($left + ($right - $left) / 2);
-        if ($this->isBadVersion($mid)) {
+
+        if (isBadVersion($mid)) {
             $right = $mid;
         } else {
             $left = $mid + 1;
         }
     }
     return $left;
+}
+
+// Test cases to be run
+$testCases = [5, 100, 444];
+foreach ($testCases as $testCase) {
+    $sol = firstBadVersion(5);
+    echo "Then $sol is the first bad version for input: $testCase" . PHP_EOL;
 }
